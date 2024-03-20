14b2ed29ee2da8ebded3a59533d2f99e863fbdb6
De1eF <jatfun58@gmail.com>
2024-03-20 16:42:36 +0200

    Initial commit

---
+++ src/test/java/org/delef/mapaapi/MapaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delef.mapaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
